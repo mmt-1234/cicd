6acdf53677a72d01ee5f7684748053c76544ef31
mmt-1234 <parkjunhyoung1234@gmail.com>
2026-04-28 00:45:00 +0900

    server error

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const app = require('./app');
 // 시스템 환경 변수에서 포트를 읽어오거나 기본값 3000을 사용
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' ,() => {
     console.log(`Production Server is continuously running on port ${PORT}`);
 });
